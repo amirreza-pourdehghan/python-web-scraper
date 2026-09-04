d49db63bdb2b7900078bbc1cf23301d80cdf1c8e
Amirreza Pourdehghan <amirrezapourdehghan301@gmail.com>
2026-09-04 19:05:39 +0330

    Update csv_storage.py

--- storage/csv_storage.py
+++ storage/csv_storage.py
@@ -1,36 +1,64 @@
-"""This Module is used for storing scraped data in CSV file."""
+"""This module is used for creating reports from scraped data."""
 
-import csv
+from fpdf import FPDF
+
+from core.scraper import Scraper
 
 
-class CSVStorage:
-    """This is CsvStorage class and it will manage your data in CSV."""
+class Reporter:
+    """This is Reporter class and it will return a report of scraped data."""
 
-    @staticmethod
-    def save(data: list[dict], filename: str) -> None:
-        """This function saves your data in a CSV file with your filename."""
-        if filename.endswith(".csv"):
-            filename = filename.replace(".csv", "")
+    def __init__(self, scraper: Scraper):
+        self.scraper = scraper
 
-        with open(f"{filename}.csv", "w", newline="", encoding="utf-8") as file:
-            if data[0].get("Tag") == "a":
-                fieldnames = ["Tag", "Text", "Href"]
-            else:
-                fieldnames = ["Tag", "Text"]
+    def make_report(self) -> str:
+        """This function makes report of scraped data."""
+        url = self.scraper.url
+        self.scraper.fetch()
+        titles = self.scraper.get_titles()
+        titles_num = len(titles)
+        links = self.scraper.get_links()
+        links_num = len(links)
 
-            csv_writer = csv.DictWriter(file, delimiter="|", fieldnames=fieldnames)
+        output = (
+            "Scraping Report"
+            "\n=================="
+            f"\n\n- URL: {url}"
+            f"\n- Total Titles: {titles_num}\n"
+            f"- Total Links: {links_num}\n"
+            "\n\nTop Titles:\n"
+        )
 
-            csv_writer.writeheader()
+        for title in titles:
+            output += f"- {title['Text']}\n"
 
-            csv_writer.writerows(data)
+        output += "\n\n\nTop Links:\n"
 
-    @staticmethod
-    def load(filename: str) -> list[dict]:
-        """This function loads your data from CSV file that you give filename."""
-        if filename.endswith(".csv"):
-            filename = filename.replace(".csv", "")
+        for link in links:
+            output += f"- {link['Text']} ({link['Href']})\n"
 
-        with open(f"{filename}.csv", "r", encoding="utf-8", newline="") as file:
-            csv_reader = csv.DictReader(file, delimiter="|")
+        return output
 
-        return list(csv_reader)
+    def export_text(self, filename: str) -> None:
+        """This function writes the report in a txt file."""
+        if filename.endswith(".txt"):
+            filename = filename.replace(".txt", "")
+
+        report = self.make_report()
+
+        with open(f"{filename}.txt", "w", encoding="utf-8") as file:
+            file.write(report)
+
+    def export_pdf(self, filename: str) -> None:
+        """This function writes the report in a PDF file."""
+        if filename.endswith(".pdf"):
+            filename = filename.replace(".pdf", "")
+
+        report = self.make_report()
+
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.add_font("IranYekan", "", "./assets/fonts/IranYekan.ttf", uni=True)
+        pdf.set_font("IranYekan", size=12)
+        pdf.multi_cell(0, 10, report)
+        pdf.output(f"{filename}.pdf")
